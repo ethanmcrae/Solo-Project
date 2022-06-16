f02a289a9ee90dd9e5237d5e327af413fb144181
ethanmcrae <ethanmcrae0@gmail.com>
2022-06-16 00:46:59 -0600

    dotenv

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const PG_URI = 'postgres://sfdhdmou:fmNNCXEdmJgPv7230c6FzyEQs3ZHv41j@heffalump.db.elephantsql.com/sfdhdmou';
+const PG_URI = process.env.PG_URI;
 
 // Create a new pool here using the connection string above
 const pool = new Pool({
